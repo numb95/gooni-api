544040c381b747e7508d597a988903ec40e21c63
AmirHossein Goodarzi <a.goodarzi@alibaba.ir>
2021-01-01 19:01:37 +0330

    healthcheck test

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+
+
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "ok"})
 	})
+
 	r.Run()
 }
